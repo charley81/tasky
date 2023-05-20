7c8341e3cb8218b7e96e9caca7b57ffa11c8d1f1
ChrisKnowsDev <chrisharley81@gmail.com>
2023-05-20 19:43:08 -0400

    user signin api route complete

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,2 +1,43 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import User from '@/models/user'
+import { connectDB } from '@/utils/database'
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email })
+      session.user.id = sessionUser._id.toString()
+
+      return session
+    },
+    async signIn(account, profile, user, credentials) {
+      try {
+        await connectDB()
+
+        const userExist = await User.findOne({ email: profile.email })
+
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture
+          })
+        }
+
+        return true
+      } catch (error) {
+        console.log('Error checking if user exist', error.message)
+        return false
+      }
+    }
+  }
+})
+
+export { handler as GET, handler as POST }
